8ff237df890285f74abc129fb4afa599edbed940
Nikita <nikita.200210@mail.ru>
2025-03-06 14:58:44 +0300

    formatted the code according to PEP 8

--- bot.py
+++ bot.py
@@ -1,7 +1,14 @@
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQueryHandler, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    MessageHandler,
+    filters,
+    CallbackQueryHandler,
+    CommandHandler,
+)
 
 from gpt import *
 from util import *
+
 
 async def start(update, context):
     dialog.mode = "main"
@@ -9,14 +16,18 @@ async def start(update, context):
     await send_photo(update, context, "main")
     await send_text(update, context, text)
 
-    await show_main_menu(update, context, {
-        "start": "главное меню бота",
-        "profile": "генерация Tinder-профля 😎",
-        "opener": "сообщение для знакомства 🥰",
-        "message": "переписка от вашего имени 😈",
-        "date": "переписка со звездами 🔥",
-        "gpt": "задать вопрос чату GPT 🧠",
-    })
+    await show_main_menu(
+        update,
+        context,
+        {
+            "start": "главное меню бота",
+            "profile": "генерация Tinder-профля 😎",
+            "opener": "сообщение для знакомства 🥰",
+            "message": "переписка от вашего имени 😈",
+            "date": "переписка со звездами 🔥",
+            "gpt": "задать вопрос чату GPT 🧠",
+        },
+    )
 
 
 async def gpt(update, context):
@@ -37,13 +48,18 @@ async def date(update, context):
     dialog.mode = "date"
     text = load_message("date")
     await send_photo(update, context, "date")
-    await send_text_buttons(update, context, text, {
-        "date_grande": "Ариана Гранде",
-        "date_robbie": "Марго Робби",
-        "date_zendaya": "Зендея",
-        "date_gosling": "Райан Гослинг",
-        "date_hardy": "Том Харди",
-    })
+    await send_text_buttons(
+        update,
+        context,
+        text,
+        {
+            "date_grande": "Ариана Гранде",
+            "date_robbie": "Марго Робби",
+            "date_zendaya": "Зендея",
+            "date_gosling": "Райан Гослинг",
+            "date_hardy": "Том Харди",
+        },
+    )
 
 
 async def date_dialog(update, context):
@@ -58,7 +74,11 @@ async def date_button(update, context):
     await update.callback_query.answer()
 
     await send_photo(update, context, query)
-    await send_text(update, context, "Отличный выбор! Пригласите этого человека на свидание за 5 сообщений")
+    await send_text(
+        update,
+        context,
+        "Отличный выбор! Пригласите этого человека на свидание за 5 сообщений",
+    )
 
     prompt = load_prompt(query)
     chatgpt.set_prompt(prompt)
@@ -68,10 +88,15 @@ async def message(update, context):
     dialog.mode = "message"
     text = load_message("message")
     await send_photo(update, context, "message")
-    await send_text_buttons(update, context, text, {
-        "message_next": "Следующее сообщение",
-        "message_date": "Пригласить на свидание"
-    })
+    await send_text_buttons(
+        update,
+        context,
+        text,
+        {
+            "message_next": "Следующее сообщение",
+            "message_date": "Пригласить на свидание",
+        },
+    )
     dialog.list.clear()
 
 
@@ -108,23 +133,27 @@ async def profile_dialog(update, context):
     dialog.count += 1
 
     if dialog.count == 1:
-        dialog.user['age'] = text
+        dialog.user["age"] = text
         await send_text(update, context, "Кем вы работаете?")
     elif dialog.count == 2:
-        dialog.user['occupation'] = text
+        dialog.user["occupation"] = text
         await send_text(update, context, "У вас есть хобби?")
     elif dialog.count == 3:
-        dialog.user['hobby'] = text
+        dialog.user["hobby"] = text
         await send_text(update, context, "Что вам НЕ нравится в людях?")
     elif dialog.count == 4:
-        dialog.user['annoys'] = text
+        dialog.user["annoys"] = text
         await send_text(update, context, "Цели знакомства?")
     elif dialog.count == 5:
-        dialog.user['goals'] = text
+        dialog.user["goals"] = text
         prompt = load_prompt("profile")
         user_info = dialog_user_info_to_str(dialog.user)
 
-        message = await send_text(update, context, "ChatGPT занимается генерацией вашего профиля. Подождите...")
+        message = await send_text(
+            update,
+            context,
+            "ChatGPT занимается генерацией вашего профиля. Подождите...",
+        )
         answer = await chatgpt.send_question(prompt, user_info)
         await message.edit_text(answer)
 
@@ -162,7 +191,11 @@ async def opener_dialog(update, context):
         prompt = load_prompt("opener")
         user_info = dialog_user_info_to_str(dialog.user)
 
-        message = await send_text(update, context, "ChatGPT занимается генерацией вашего профиля. Подождите...")
+        message = await send_text(
+            update,
+            context,
+            "ChatGPT занимается генерацией вашего профиля. Подождите...",
+        )
         answer = await chatgpt.send_question(prompt, user_info)
         await message.edit_text(answer)
 
@@ -184,10 +217,12 @@ async def hello(update, context):
         await send_text(update, context, "Вы написали: " + update.message.text)
 
         await send_photo(update, context, "avatar_main")
-        await send_text_buttons(update, context, "*Запустить процесс?*", {
-            "start": "Запустить",
-            "stop": "Остановить"
-        })
+        await send_text_buttons(
+            update,
+            context,
+            "*Запустить процесс?*",
+            {"start": "Запустить", "stop": "Остановить"},
+        )
 
 
 async def hello_button(update, context):
@@ -214,7 +249,7 @@ app.add_handler(CommandHandler("message", message))
 app.add_handler(CommandHandler("profile", profile))
 app.add_handler(CommandHandler("opener", opener))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
-app.add_handler(CallbackQueryHandler(date_button, pattern='^date_ *'))
-app.add_handler(CallbackQueryHandler(message_button, pattern='^message_ *'))
+app.add_handler(CallbackQueryHandler(date_button, pattern="^date_ *"))
+app.add_handler(CallbackQueryHandler(message_button, pattern="^message_ *"))
 app.add_handler(CallbackQueryHandler(hello_button))
 app.run_polling()
