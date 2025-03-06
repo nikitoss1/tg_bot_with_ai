480021dcc432ceaee9560107bf996c4ecfd86c24
Nikita <nikita.200210@mail.ru>
2025-03-06 11:44:38 +0300

    add gpt

--- bot.py
+++ bot.py
@@ -4,21 +4,49 @@ from gpt import *
 from util import *
 
 async def start(update, context):
+    dialog.mode = "main"
     text = load_message("main")
     await send_photo(update, context, "main")
     await send_text(update, context, text)
 
+    await show_main_menu(update, context, {
+        "start": "главное меню бота",
+        "profile": "генерация Tinder-профля 😎",
+        "opener": "сообщение для знакомства 🥰",
+        "message": "переписка от вашего имени 😈",
+        "date": "переписка со звездами 🔥",
+        "gpt": "задать вопрос чату GPT 🧠",
+    })
+
+
+async def gpt(update, context):
+    dialog.mode = "gpt"
+    text = load_message("gpt")
+    await send_photo(update, context, "gpt")
+    await send_text(update, context, text)
+
+
+async def gpt_dialog(update, context):
+    text = update.message.text
+    prompt = load_prompt("gpt")
+    answer = await chatgpt.send_question(prompt, text)
+    await send_text(update, context, answer)
+
+
 
 async def hello(update, context):
-    await send_text(update, context, "*Привет*")
-    await send_text(update, context, "_Как дела_")
-    await send_text(update, context, "Вы написали: " + update.message.text)
+    if dialog.mode == "gpt":
+        await gpt_dialog(update, context)
+    else:
+        await send_text(update, context, "*Привет*")
+        await send_text(update, context, "_Как дела_")
+        await send_text(update, context, "Вы написали: " + update.message.text)
 
-    await send_photo(update, context, "avatar_main")
-    await send_text_buttons(update, context, "*Запустить процесс?*", {
-        "start": "Запустить",
-        "stop": "Остановить"
-    })
+        await send_photo(update, context, "avatar_main")
+        await send_text_buttons(update, context, "*Запустить процесс?*", {
+            "start": "Запустить",
+            "stop": "Остановить"
+        })
 
 
 async def hello_button(update, context):
@@ -29,8 +57,14 @@ async def hello_button(update, context):
         await send_text(update, context, "*Вы нажали кнопку стоп*")
 
 
+dialog = Dialog()
+dialog.mode = None
+
+chatgpt = ChatGptService(token="")
+
 app = ApplicationBuilder().token("").build()
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("gpt", gpt))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
 app.add_handler(CallbackQueryHandler(hello_button))
 app.run_polling()
